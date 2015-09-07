11c2580d0059825a47b0bd18dedd56be813d0193
Ahmad Alfy <ahmadalfy@gmail.com>
2015-09-07 10:41:44 +0200

    Initial Commit
    This is my grunt workflow that I use to build html websites. I am using:
    
    - Jade templates for HTML
    - Sass
    - SVG sprites
    - and a lot more ...

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+/* global GMaps */
+'use strict';
+$(document).ready(function(){
+
+  // Injecting SVG-sprite
+
+  /*
+  var svgPath = 'images/svg-defs.svg';
+  $.get(svgPath, function(data) {
+    var $icons = $('<div class="icons"></div>').html(new XMLSerializer().serializeToString(data.documentElement));
+    $('body').prepend($icons);
+  });
+  */
+
+});
